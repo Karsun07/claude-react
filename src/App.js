15ac23d94976a31fde75d31dd43e68eac358f9c1
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-05-22 15:37:19 +0530

    structure made

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";  
+import ReactDOM from "react-dom/client";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+function App() {
+  return (
+    <div className="flex h-screen overflow-hidden">
 
+      {/* Sidebar */}
+      <div className="hidden md:flex w-64 bg-[#1e1f23] border-r border-gray-700">
+        <Sidebar />
+      </div>
+
+      {/* Main */}
+      <div className="flex-1 bg-[#2b2d31] overflow-y-auto">
+        <MainContent />
+      </div>
+
+    </div>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
